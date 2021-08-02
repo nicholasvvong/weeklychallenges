8a46f5a4db681d015f127b8d75d125feb1fd03a0
nicholasvvong <nicholasvvong@gmail.com>
2021-08-02 13:38:00 -0700

    20210802 challenges

---
+++ 20210802/problem_one/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace problem_one
+{
+    class Program
+    {
+        static int LongestCommonSubsequences(string shorter, string longer)
+        {
+            for(int i = 0; i < shorter.Length; i++)
+            {
+                for(int j = shorter.Length; j > i; j--)
+                {
+                    string subStr = shorter.Substring(i, j);
+                    if(longer.Contains(subStr))
+                        return j - i;
+                }
+            }
+            return -1;
+        }
+
+        static void Main(string[] args)
+        {
+            string first, second;
+            int length;
+            Console.Write("Enter first string: ");
+            first = Console.ReadLine();
+            Console.Write("Enter second string: ");
+            second = Console.ReadLine();
+            if(first.Length > second.Length)
+                length = LongestCommonSubsequences(second, first);
+            else
+                length = LongestCommonSubsequences(first, second);
+            if(length == -1)
+                Console.WriteLine("Could not find any matching subsequences.");
+            else
+                Console.WriteLine($"The length of the longest common subsequence is {length}");
+        }
+    }
+}
---
+++ 20210802/problem_two/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace problem_two
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> arrList = new List<int>{
+                5,3,1,2
+            };
+            List<int> missingElements = new List<int>();
+            arrList.Sort();
+            Console.WriteLine($"Smallest element: {arrList[0]}");
+            Console.WriteLine($"Largest element: {arrList[arrList.Count - 1]}");
+            for(int i = 0; i < arrList.Count - 1; i++)
+            {
+                if(arrList[i+1] != arrList[i] + 1)
+                    missingElements.Add(arrList[i] + 1);
+            }
+            Console.WriteLine($"Missing numbers: {String.Join(' ', missingElements)}");
+        }
+    }
+}
